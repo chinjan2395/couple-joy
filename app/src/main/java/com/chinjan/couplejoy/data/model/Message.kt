3e03e2e1e1e5405a271e19644c508bdb4e4656a2
chinjanpatel <chinjan@keeway.com>
2025-04-24 00:09:57 +0530

    Added Message model with Timestamp
    Signed-off-by: chinjanpatel <chinjan@keeway.com>

---
+++ app/src/main/java/com/chinjan/couplejoy/data/model/Message.kt
@@ -0,0 +1,10 @@
+package com.chinjan.couplejoy.data.model
+
+import com.google.firebase.Timestamp
+
+data class Message(
+    val text: String = "",
+    val timestamp: Timestamp = Timestamp.now(),
+    val device: String = "",
+    val uid: String = "",
+)
