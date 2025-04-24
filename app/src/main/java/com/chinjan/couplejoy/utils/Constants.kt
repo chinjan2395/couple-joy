4244e844b212bdaf3c4c02f62e31993a12e0cd25
chinjanpatel <chinjan@keeway.com>
2025-04-24 12:50:12 +0530

    Created Constants file for shared preferences keys and Firestore collection names.
    Signed-off-by: chinjanpatel <chinjan@keeway.com>

---
+++ app/src/main/java/com/chinjan/couplejoy/utils/Constants.kt
@@ -0,0 +1,21 @@
+package com.chinjan.couplejoy.utils
+
+object Constants {
+    // SharedPreferences Keys
+    const val PREFS_NAME = "couple_prefs"
+    const val PREF_ROLE = "partner_role"
+    const val PREF_COUPLE_ID = "couple_id"
+
+    // Firestore
+    const val COLLECTION_UID = "uid"
+    const val COLLECTION_COUPLES = "couples"
+    const val COLLECTION_MESSAGES = "messages"
+    const val COLLECTION_MESSAGE = "message"
+    const val COLLECTION_DEVICES = "devices"
+    const val PARTNER_A = "partnerA"
+    const val PARTNER_B = "partnerB"
+
+    // Widget
+    const val WIDGET_MESSAGE_KEY = "widget_message"
+
+}
