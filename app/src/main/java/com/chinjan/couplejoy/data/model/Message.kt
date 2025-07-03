b8476148cc0ebbc6c776257fe2003079a8eea6f3
Chinjan Patel <patel.chinjan531@gmail.com>
2025-07-03 19:58:05 +0530

    Refactor: Rename text field to message in Message model
    The `text` field in the `Message` data class has been renamed to `message` for clarity.
    
    Signed-off-by: Chinjan Patel <patel.chinjan531@gmail.com>

--- app/src/main/java/com/chinjan/couplejoy/data/model/Message.kt
+++ app/src/main/java/com/chinjan/couplejoy/data/model/Message.kt
@@ -3,7 +3,7 @@ package com.chinjan.couplejoy.data.model
 import com.google.firebase.Timestamp
 
 data class Message(
-    val text: String = "",
+    val message: String = "",
     val timestamp: Timestamp = Timestamp.now(),
     val device: String = "",
     val uid: String = "",
